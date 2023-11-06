8d4aa20365845e0488a127e162be5443454e7807
Xin <xzhao34@slb.com>
2023-11-06 21:24:21 +0800

    feat: add EmplyeeRequest class

--- CompanyApi/Employee.cs
+++ CompanyApi/Employee.cs
@@ -2,16 +2,8 @@
 {
     public class Employee
     {
-        public Employee(string name)
-        {
-            Id = Guid.NewGuid().ToString();
-            Name = name;
-        }
-
-        public string Id { get; set; }
-
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
-
         public double Salary { get; set; }
     }
 }
---
+++ CompanyApi/EmployeeRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace CompanyApi
+{
+    public class EmployeeRequest
+    {
+        public EmployeeRequest(double salary, string name)
+        {
+            Salary = salary;
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
